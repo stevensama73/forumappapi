e8b7dc7248bc5a94975d9fb8f24ed2c80605aa17
stevensama73 <stevensama73@gmail.com>
2023-11-04 12:00:04 +0700

    finish likes comment

---
+++ client.js
@@ -0,0 +1,30 @@
+/* istanbul ignore file */
+const { Client } = require('pg');
+
+const testConfig = new Client({
+  host: process.env.PGHOST_TEST,
+  port: process.env.PGPORT_TEST,
+  user: process.env.PGUSER_TEST,
+  password: process.env.PGPASSWORD_TEST,
+  database: process.env.PGDATABASE_TEST,
+});
+
+testConfig.connect();
+
+const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
+const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
+const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
+
+pgclient.query(table, (err, res) => {
+    if (err) throw err
+});
+
+pgclient.query(text, values, (err, res) => {
+    if (err) throw err
+});
+
+pgclient.query('SELECT * FROM student', (err, res) => {
+    if (err) throw err
+    console.log(err, res.rows) // Print the data in student table
+    pgclient.end()
+});
