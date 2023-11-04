438ad68c5dd45e9639234838f34898eecf375955
stevensama73 <stevensama73@gmail.com>
2023-11-04 13:09:36 +0700

    finish likes comment

--- client.js
+++ client.js
@@ -4,8 +4,8 @@ const { Client } = require('pg');
 const pgclient = new Client({
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
-  user: 'postgres',
-  password: 'postgres',
+  user: 'developer',
+  password: 'supersecretpassword',
   database: 'forumapi_test'
 });
 
