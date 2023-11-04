7ad921258633bebacb18799853a98daae08dddd9
stevensama73 <stevensama73@gmail.com>
2023-11-04 12:07:35 +0700

    up

--- client.js
+++ client.js
@@ -15,16 +15,16 @@ const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40)
 const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
 const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
 
-pgclient.query(table, (err, res) => {
+testConfig.query(table, (err, res) => {
     if (err) throw err
 });
 
-pgclient.query(text, values, (err, res) => {
+testConfig.query(text, values, (err, res) => {
     if (err) throw err
 });
 
-pgclient.query('SELECT * FROM student', (err, res) => {
+testConfig.query('SELECT * FROM student', (err, res) => {
     if (err) throw err
     console.log(err, res.rows) // Print the data in student table
-    pgclient.end()
+    testConfig.end()
 });
