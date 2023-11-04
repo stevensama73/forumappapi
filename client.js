c6d8ddffa2e264a0367306d8990bc408a4ebd994
stevensama73 <stevensama73@gmail.com>
2023-11-04 12:16:44 +0700

    up

--- client.js
+++ client.js
@@ -1,7 +1,30 @@
-const pgclient = new Client({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: 'postgres',
-  password: 'postgres',
-  database: 'postgres'
+/* istanbul ignore file */
+const { Client } = require('pg');
+
+const testConfig = new Client({
+  host: process.env.PGHOST_TEST,
+  port: process.env.PGPORT_TEST,
+  user: process.env.PGUSER_TEST,
+  password: process.env.PGPASSWORD_TEST,
+  database: process.env.PGDATABASE_TEST,
+});
+
+testConfig.connect();
+
+const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
+const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
+const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
+
+testConfig.query(table, (err, res) => {
+    if (err) throw err
+});
+
+testConfig.query(text, values, (err, res) => {
+    if (err) throw err
+});
+
+testConfig.query('SELECT * FROM student', (err, res) => {
+    if (err) throw err
+    console.log(err, res.rows) // Print the data in student table
+    testConfig.end()
 });
